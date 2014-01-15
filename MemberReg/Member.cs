38712035013853923d75d08dfd8986a3991b0b40
je222ks <je222ks@student.lnu.se>
2014-01-15 22:09:54 +0100

    påbörjat kodning - dock sen commit pga en del omarbete

---
+++ MemberReg/Member.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberReg
+{
+    class Member
+    {
+        private string _firstName;
+        private string _lastName;
+        private int _telNumber;
+        private int memberId = 0;
+        List<object> _memberList = new List<object>();
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set 
+            {
+                if (String.IsNullOrWhiteSpace(value)) 
+                {
+                    throw new ArgumentException();
+                }
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                _lastName = value;
+            }
+        }
+
+        public int TelNumber
+        {
+            get { return _telNumber; }
+            set
+            {
+                if (value.GetType() != typeof(int))
+                {
+                    throw new ArgumentException();
+                }
+                _telNumber = value;
+            }
+        }
+
+        public int MemberId
+        {
+            get { return memberId; }
+        }
+
+        public ReadOnlyCollection<object> MemberList
+        {
+            get { return _memberList.AsReadOnly(); }
+        }
+
+        public Member(string firstName, string lastName, int telNumber) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            TelNumber = telNumber;
+        }
+
+        public void Add(object member)
+        {
+            _memberList.Add(member);
+            memberId++;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("{0}\t{1}\tTel-Nummer: {2}\t Medlemsnummer: {3}", FirstName, LastName, TelNumber, MemberId);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ MemberReg/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberReg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> memberCount = new List<object>();
+
+            do
+            {
+                switch (GetMenuChoice())
+                {
+                    case 0:
+                        return;
+
+                    case 1:
+                        
+                        break;
+
+                    case 2:
+                        
+                        break;
+
+                    case 3:
+                        
+                        break;
+
+                    case 4:
+                        
+                        break;
+
+                    default:
+                        throw new Exception();
+                }
+            } while (true);
+        }
+
+        public static void createMember() 
+        {
+            string fName;
+            string lName;
+            int telNr;
+
+            Console.Write("Ange förnamn: ");
+            fName = Console.ReadLine();
+
+            Console.Write("Ange efternamn: ");
+            lName = Console.ReadLine();
+
+            Console.Write("Ange tel-nummer: ");
+            telNr = int.Parse(Console.ReadLine());
+
+            Member newMember = new Member(fName, lName, telNr);
+
+            newMember.Add(newMember);
+
+            foreach (object mem in newMember.MemberList) 
+            {
+                Console.WriteLine(mem);
+            }
+
+
+        }
+
+        private static int GetMenuChoice()
+        {
+            int index;
+
+            // Inläsning av menyval, och kontroll så att det är ett giltigt val.
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("|            Medlemsregister           |");
+            Console.WriteLine("----------------------------------------");
+            Console.ResetColor();
+
+            Console.WriteLine("\n - Arkiv -----------------------------\n");
+            Console.WriteLine(" 0. Avsluta \n");
+            Console.WriteLine(" 1. Lägg till ny medlem \n");
+            Console.WriteLine(" 2. Redigera medlem \n");
+            Console.WriteLine(" 3. Ta bort medlem \n");
+            Console.WriteLine(" 4. Visa alla medlemmar \n");
+            Console.WriteLine("\n ========================================\n");
+            Console.Write("Ange menyval [0-5]: ");
+
+            do
+            {
+                try
+                {
+                    index = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("\nVad du har matat in är inte ett giltigt menyval.\n");
+                    Console.WriteLine("\n\tVar god försök igen\t\n");
+                    Console.Write("Ange menyval [0-4]: ");
+                    Console.ResetColor();
+                }
+
+            } while (true);
+
+            return index;
+        }
+
+    }
+}
